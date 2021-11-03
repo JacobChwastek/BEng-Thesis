3b026637064182ffe985afcb025b7ed147c017dc
Jacob Chwastek <jacobchwastek@gmail.com>
2021-11-03 21:17:48 +0100

    Remove ignored files

--- Dicom.API/Dicom.API/Controllers/UserController.cs
+++ Dicom.API/Dicom.API/Controllers/UserController.cs
@@ -16,14 +16,14 @@ namespace Dicom.API.Controllers
             CreateUserAsync([FromBody] CreateUserCommand createUser)
         {
             var result = await Mediator.Send(createUser);
-            return result != null ? Created("", result) : (IActionResult)BadRequest(result);
+            return result != null ? Created("", result) : BadRequest(result);
         }
 
         [HttpPost("login")]
         public async Task<IActionResult> LoginAsync([FromBody] LoginCommand login)
         {
             var result = await Mediator.Send(login);
-            return result != null ? Created("", result) : (IActionResult)BadRequest(result);
+            return result != null ? Created("", result) : BadRequest(result);
         }
 
 
